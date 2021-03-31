92c4028226515e955e5e370dead99a931c9b59f5
Plasnid <stuart@sackler.ca>
2021-03-31 13:45:44 -0400

    initial files

---
+++ script.js
@@ -0,0 +1,38 @@
+/**
+ * * Q1.  30 Marks
+ * * Create a self executing function that returns an object describing an animal of your choice.
+ * * Ensure your animal object contains the following information
+ * * -weight
+ * * -covering
+ * * -length
+ * * -diet
+ * * -predators
+ * * -habitat
+ * * -source information web page
+ * 
+ * * Your object needs to contain at least:
+ * * -1 array containing 3 or more entries
+ * * -1 or more strings
+ * * -1 or more numbers
+ * * -1 or more booleans
+ */
+
+/**
+ * * Q2. 20 marks
+ * * Create a function expression that takes the output of question 1 and
+ * * loops backwards through the array in the object console logging each time. 
+ */
+
+/**
+ * * Q3. 25 marks
+ * * Create a fat arrow function that takes in the output of question 1 and
+ * * returns a string in the following format: The __animalName__ lives in __habitat__ and has a covering of __covering__.
+ * * Ensure that your function is a single line.
+ */
+
+/**
+ * * Q4. 25 marks
+ * * Create a function declaration that takes in the output of question 1, and loops through
+ * * all of it's values, console logging each individual key:value pair.  
+ * * To answer this question review how to loop through an object.
+ */
