284f2a7c5a95cdec39ba586c5719085fe87a4956
Plasnid <stuart@sackler.ca>
2021-03-31 14:39:50 -0400

    answers added

--- script.js
+++ script.js
@@ -17,11 +17,46 @@
  * * -1 or more booleans
  */
 
+let animalFacts = (function(){
+    return {
+        name: "bilby",
+        otherNames: ["greater bilby", "dalgyte", "greater rabbit-eared bandicoot"],
+        nativeTo: "Australia",
+        currentlyLimitedTo: ["Great Sandy", "Tanami", "Gibson deserts", "southwestern Queensland"],
+        family: "Peramelidae",
+        sexuallyDimorphic: true,
+        aproximateWeightinKg: 2.5,
+        description: "The bilby is known for its long snout, blue-gray fur, white underbelly, and long, hairless ears that resemble those of rabbits. On its tail is a prominent band of black fur that terminates with a white tuft that surrounds a naked, spurlike tip.",
+        maximumLengthInCM: 55,
+        GraspingApendages: "The ends of the forelimbs terminate in five digits with the middle three digits possessing a claw.",
+        locomotion: "loping",
+        sourceReference: "https://www.britannica.com/animal/bilby",
+        hasPouch: true,
+        pouchFacing: "reverse",
+        diet: "omnivore",
+        nativePredators: ["wedge-tailed eagles (Aquila audax)", "carpet pythons (Morelia spilota)", "monitor lizards (family Varanidae)"],
+        invasivePredators:["red foxes", "feral cats", "dingoes"],
+        livesIn: "burrows",
+        solitary: true,
+        littersPerYear: 4,
+        gestationPeriod: "12 to 14 days",
+        pouchPeriod: "75-80 days",
+        lifeSpan: "6 to 11 years",
+        convervationStatus: "vulnerable",
+        covering: "fur"
+    }
+})();
 /**
  * * Q2. 20 marks
  * * Create a function expression that takes the output of question 1 and
  * * loops backwards through the array in the object console logging each time. 
  */
+let factLoop = function(facts){
+    for(let i=facts.nativePredators.length-1; i>=0; i--){
+        console.log(facts.nativePredators[i]);
+    }
+}
+factLoop(animalFacts)
 
 /**
  * * Q3. 25 marks
@@ -29,6 +64,8 @@
  * * returns a string in the following format: The __animalName__ lives in __habitat__ and has a covering of __covering__.
  * * Ensure that your function is a single line.
  */
+let habitatReturner = animal => `The ${animal.name} lives in ${animal.currentlyLimitedTo[0]} and has a covering of ${animal.covering}`;
+console.log(habitatReturner(animalFacts));
 
 /**
  * * Q4. 25 marks
@@ -36,3 +73,9 @@
  * * all of it's values, console logging each individual key:value pair.  
  * * To answer this question review how to loop through an object.
  */
+function animalObjLooper(animal){
+    for(feature in animal){
+        console.log(`${feature}: ${animal[feature]}`);
+    };
+}
+animalObjLooper(animalFacts);
